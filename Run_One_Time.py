6aee07d3544aa6a338c9c32cf63349e5f98b4dc0
advaitmenon1106 <advaitmenon1106@gmail.com>
2024-07-04 00:43:12 +0530

    Migrate to a NoSQL database (MongoDB) due to the complications faced by the lack of a hosted SQL website for the website.

--- Run_One_Time.py
+++ Run_One_Time.py
@@ -1,16 +1,29 @@
-import psycopg2 as psg
 import pandas as pd
 import streamlit as st
-conn = psg.connect('dbname = indian_tbill_data', user=st.secrets['db_username'], password=st.secrets['db_password'])
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
-with conn:
-    with conn.cursor() as curs:
-        curs.execute('CREATE TABLE tbill_data (date DATE NOT NULL PRIMARY KEY, yield NUMERIC)')
-        df = pd.read_csv('./TBill-10Y.csv')
-        df.set_index('Date', inplace=True)
+uri = f"mongodb+srv://{st.secrets['mongo_username']}:{st.secrets['mongo_pw']}@cluster0.xhu6g0i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+client = MongoClient(uri, server_api=ServerApi('1'))
 
-        for i in range(0, len(df.index)):
-            date_to_be_added = list(df.index)[i]
-            ytm_to_be_added = df.loc[date_to_be_added]
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 
-            curs.execute('INSERT INTO tbill_data (date, yield) VALUES (%s, %s)', (date_to_be_added, float(ytm_to_be_added.values)))
+tbill_db = client['tbill_data']
+indian_tbill_data = tbill_db['indian_tbill_data']
+
+df = pd.read_csv('./TBill-10Y.csv')
+df.set_index('Date', inplace=True)
+print(df)
+
+observations = []
+
+for i in range (len(df.index)):
+    date_to_be_inserted = df.index[i]
+    yield_to_be_inserted = df.loc[date_to_be_inserted]
+    observations.append({'Date': date_to_be_inserted, 'Yield': float(yield_to_be_inserted.values)})
+
+indian_tbill_data.insert_many(observations)
